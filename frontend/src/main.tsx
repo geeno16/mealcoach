951aedff8a82c7b74a8f37d774ab567327d7d50c
Zakhar Putivtsev <send.zpko@gmail.com>
2026-06-07 15:04:03 +0300

    Create navigation docs

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import { App } from "./App";
-import { StoreProvider } from "./stores/StoreContext";
+import { StoreProvider } from "./root_store/StoreContext";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
